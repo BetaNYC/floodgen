d326244244738a4eaf65891eff35efefd1d59091
CloudLun <cloud830902@gmail.com>
2024-02-08 14:42:00 -0500

    change the window provider to useMediaquery

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Image from 'next/image'
 
-import { WindowProvider } from '@/contexts/windowContext'
 import { MapProvider } from '@/contexts/MapContext'
 import Map from '@/components/map/Map'
 import InfoPage from '@/components/infoPage/InfoPage'
@@ -11,14 +10,12 @@ import StreetInfo from '@/components/streetView/StreetInfo'
 export default function Home() {
   return (
     <main className="relative w-[100vw] h-[100vh] overflow-y-hidden">
-      <WindowProvider>
-        <MapProvider>
-          <Map />
-          <InfoPage />
-          <StreetView />
-          <StreetInfo />
-        </MapProvider>
-      </WindowProvider>
+      <MapProvider>
+        <Map />
+        <InfoPage />
+        <StreetView />
+        <StreetInfo />
+      </MapProvider>
     </main>
   )
 }
--- components/infoPage/infoBox/InfoBox.tsx
+++ components/infoPage/infoBox/InfoBox.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 
-import { WindowContext, WindowContextType } from '@/contexts/windowContext'
+import { useMediaQuery } from 'react-responsive'
+
 
 // import InfoTitle from './InfoTitle'
 import InfoAbout from './InfoAbout'
@@ -12,30 +13,28 @@ import Image from 'next/image'
 
 const InfoBox = () => {
 
-    const { desktop, width, setWidth } = useContext(WindowContext) as WindowContextType
 
     const [selected, setSelected] = useState<'About' | "Credit">("About")
     const [boxShown, setBoxShown] = useState(false)
+
+
+    const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" })
 
 
     const selectedClickHandler = (s: 'About' | "Credit") => {
         setSelected(s)
     }
 
-    const boxShownClickHandler = (b:boolean) => {
+    const boxShownClickHandler = (b: boolean) => {
         setBoxShown(b)
     }
-
-
-    useEffect(() => window.addEventListener('resize', () => setWidth(window.innerWidth)))
-
 
 
     return (
         <>
             <div className={`absolute top-[36%]  lg:top-0 w-full h-[64%] lg:h-full bg-white lg:bg-secondary_blue rounded-t-[1rem] lg:rounded-none z-30 overflow-y-auto ${boxShown ? "translate-y-0 duration-700" : "translate-y-full duration-700"}`}>
                 <div className=' px-[1.125rem] lg:px-[4rem] pt-[3rem] lg:pt-[5rem] pb-[2rem] w-full rounded-t-[1rem]'>
-                    <Image width={width > desktop ? 203 : 80} height={width > desktop ? 38.17 : 15.4} src={`${width > desktop ? "./logos/floodgen.svg" : "./logos/floodgen.svg"}`} alt='floodgen' />
+                    <Image width={isDesktop ? 203 : 80} height={isDesktop ? 38.17 : 15.4} src="./logos/floodgen.svg" alt='floodgen' />
                     {/* <InfoTitle /> */}
                     <div className='lg:flex lg:mt-[4rem]'>
                         (<div className='flex lg:flex-col gap-[0.75rem] lg:gap-[1.125rem] lg:mr-[8.25rem] w-full lg:w-[12.68rem]'>
@@ -49,7 +48,7 @@ const InfoBox = () => {
                 </div>
             </div>
             <div className='absolute top-[1.75rem] left-[1rem] lg:left-[calc(100%_-_4.5rem)] flex items-center justify-center w-[2.5rem] h-[2.5rem] bg-[rgba(255,255,255,.65)] z-30' onClick={() => boxShown ? boxShownClickHandler(false) : boxShownClickHandler(true)}>
-                <Image width={18} height={12} src={boxShown ? "./icons/cross.svg":"./icons/hamburger.svg"} alt='hamburger' className='cursor-pointer' />
+                <Image width={18} height={12} src={boxShown ? "./icons/cross.svg" : "./icons/hamburger.svg"} alt='hamburger' className='cursor-pointer' />
             </div>
         </>
     )
