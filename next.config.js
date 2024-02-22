7387e4374ca285b876437ed1c45256b15a400cf5
CloudLun <cloud830902@gmail.com>
2024-02-22 01:28:43 -0500

    change the base path to floodgen

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: '/github-pages',
+    basePath: '/floodgen',
     // env: {
     //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
     // }
