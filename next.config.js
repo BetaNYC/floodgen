cb9f49464edd6c3fca32a065c2657b631fcfd517
CloudLun <cloud830902@gmail.com>
2024-02-22 00:25:10 -0500

    turn back to the output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    target: 'experimental-serverless-trace',
+    output: "export",
     // env: {
     //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
     // }
