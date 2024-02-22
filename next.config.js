276b92fea87c61fdc6d935224469900d79a79eed
CloudLun <cloud830902@gmail.com>
2024-02-22 01:21:53 -0500

    add basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: '/github-pages',
     // env: {
     //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
     // }
