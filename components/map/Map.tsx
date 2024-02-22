c58cd6b2729e0ab2ba975a0eb0afd29f6f61002b
CloudLun <cloud830902@gmail.com>
2024-02-22 00:17:17 -0500

    commented the env

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -29,7 +29,7 @@ const Map = () => {
 
 
     useEffect(() => {
-        mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN as string
+        mapboxgl.accessToken = "pk.eyJ1IjoiYmV0YW55YyIsImEiOiJhdEk0RmZ3In0.z3ayA_ZWlFP7Co7h-T-6WQ" as string
 
         const m = new mapboxgl.Map({
             container: mapContainer.current || "",
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    env: {
-        BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
-    }
+    // env: {
+    //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
+    // }
 
 }
 
