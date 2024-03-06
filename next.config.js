8f46f83a09341f7e16c707b51c6e0accf46e6d17
CloudLun <cloud830902@gmail.com>
2024-03-06 07:26:09 -0500

    add strictmode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: '/floodgen'
+    basePath: '/floodgen',
+    reactStrictMode: true,
     // env: {
     //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
     // }
