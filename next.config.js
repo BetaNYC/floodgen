de93a4e087afafffe7824a86a21503ea92cd0b6d
CloudLun <cloud830902@gmail.com>
2024-03-18 23:15:23 -0400

    add localhost domains in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: '',
-    // reactStrictMode: true,
-    // env: {
-    //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
-    // }
+  output: "export",
+  basePath: "",
+  // reactStrictMode: true,
+  // env: {
+  //     BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
+  // }
+  images: {
+    domains: ["localhost"],
+  },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
 
 // export default nextConfig
