0126832620634fad0d512940865cab7f50b2b25b
CloudLun <cloud830902@gmail.com>
2024-03-20 16:54:38 -0400

    add export in config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  // basePath: "",
+  output: "export",
+  basePath: "",
   // reactStrictMode: true,
   env: {
       BASE_URL: process.env.NEXT_PUBLIC_MAPBOX_API_KEY,
